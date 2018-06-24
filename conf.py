0dac619fddc1981252d49c43abacb58c0456b329
Roberto Alsina <ralsina@netmanagers.com.ar>
2018-06-24 15:04:24 -0300

    galeria 2do encuentro

--- conf.py
+++ conf.py
@@ -676,7 +676,7 @@ GITHUB_COMMIT_SOURCE = True
 # "source/" and the results will be located in
 # "OUTPUT_PATH/relative_destination/gallery_name"
 # Default is:
-# GALLERY_FOLDERS = {"galleries": "galleries"}
+GALLERY_FOLDERS = {"albums": "albums"}
 # More gallery options:
 # THUMBNAIL_SIZE = 180
 # MAX_IMAGE_SIZE = 1280
