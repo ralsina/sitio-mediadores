8745ac41874c37b541c6b4487b793e12919db01d
Roberto Alsina <roberto.alsina@gmail.com>
2020-07-26 14:15:07 -0300

    identity redirect

--- conf.py
+++ conf.py
@@ -1143,6 +1143,17 @@ $(document).ready(function() {
     });
 });
 </script>
+<script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
 """
 
 # If you prefer a Google search form, here's an example that should just work:
