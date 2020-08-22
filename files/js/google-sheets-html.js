ecb475bc2a6ca346fa31885272e508c6555568c0
Roberto Alsina <roberto.alsina@gmail.com>
2020-08-22 14:22:22 -0300

    Arreglar jurisprudencia

--- files/js/google-sheets-html.js
+++ files/js/google-sheets-html.js
@@ -4,7 +4,7 @@
 var data;
 $.ajax({
   type: "GET",
-  url: "https://docs.google.com/spreadsheets/d/1AS_fsPWZiHqagc9RONpA9ZN15dvyHm1BylgMYTudgcg/export?format=csv",
+  url: "https://docs.google.com/spreadsheets/d/1AS_fsPWZiHqagc9RONpA9ZN15dvyHm1BylgMYTudgcg/pub?output=csv",
   dataType: "text",
   success: function(response)
   {
